43fdb8017bc9cd8e867f5081069f0a4bb285b2a1
asmin <42002571+asminalev@users.noreply.github.com>
2023-11-27 01:30:22 +0300

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import snowflake.connector
 import pandas
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
 
-'''
+
 # connect to snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -19,7 +19,7 @@ df = pandas.DataFrame(my_catalog)
 # streamlit.write(df)
 # put the first column into a list
 color_list = df[0].values.tolist()
-
+'''
 # print(color_list)
 # Let's put a pick list here so they can pick the color
 option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
